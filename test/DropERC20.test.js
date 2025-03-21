db55607647d77646c7dfaed439a2db594c0082dc
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2025-03-21 18:46:52 +0800

    claim via signature test added

--- test/DropERC20.test.js
+++ test/DropERC20.test.js
@@ -2,6 +2,39 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { hexlify, toUtf8Bytes } = require('ethers')
 
+const generateEphemeralKeySig = async (ephemeralKey, recipient) => {
+  const wallet = new ethers.Wallet(ephemeralKey)
+  const abiCoder = new ethers.AbiCoder();  
+  const encodedParams = abiCoder.encode(
+    ['address'],
+    [recipient]    
+  );
+  const dataHash = ethers.keccak256(encodedParams);
+  const messageHash = ethers.hashMessage(ethers.getBytes(dataHash));
+  
+  const splitSig = wallet.signingKey.sign(messageHash);
+  const signature = ethers.Signature.from(splitSig).serialized  
+  // const recovered = ethers.recoverAddress(messageHash, signature);
+
+  return signature
+}
+
+// const verifyAllocatorSig = async (taskId, schemaId, validator, signature) => {
+//   // // Encode parameters as bytes
+//   const abiCoder = new ethers.AbiCoder();  
+//   const encodedParams = abiCoder.encode(
+//   ["bytes32", "bytes32", "address"],
+//     [taskId, schemaId, validator]
+//   );
+  
+//   // Hash the encoded parameters using keccak256
+//   const dataHash = ethers.keccak256(encodedParams);
+//   const messageHash = ethers.hashMessage(ethers.getBytes(dataHash));
+//   const recovered = ethers.recoverAddress(messageHash, signature);
+//   return { signature, recovered }
+// }
+
+
 describe("DropERC20", function () {
   let DropERC20;
   let dropERC20;
@@ -44,26 +77,61 @@ describe("DropERC20", function () {
   describe("claim", function () {
     // Test 1: Successful claim
     it("should allow a valid user to claim tokens", async function () {
-      // Mock ZKPass verification if needed
-      // This will depend on your implementation
-
       const webproof = {
-        "taskId":"e0a1b0da29554ed6823e5ae481d3e36a",
-        "publicFields":[],
-        "allocatorAddress":"0x19a567b3b212a5b35bA0E3B600FbEd5c2eE9083d",
-        "publicFieldsHash":"0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",        "allocatorSignature":"0x1adf885be2c9786a1875e29cb83baf6ce9d416ab19b08c55478514d45f5583463fcdbbcccfa4e0674210eadfdf0c45d81170ed137fee3a377c931523cb2692dc1c",
-        "uHash":"0xd4b1ee22a7a2eeb2adf53d79b7430a396ffcf699276112fa21949b8ff8fd172c",
-        "validatorAddress":"0xb1C4C1E1Cdd5Cf69E27A3A08C8f51145c2E12C6a",       "validatorSignature":"0xd517d0d3f4a10cfa6d83505c683407a675b957ed4ef8df5b218d2cbee549181267fdfb92630906650f602d2257ecf3997455bcd13502977976e7ea2155af9d541b"
+        "taskId": "0cad2ec6ef3248a992d59e2315ce38d1",
+        "publicFields": [],
+        "allocatorAddress": "0x19a567b3b212a5b35bA0E3B600FbEd5c2eE9083d",
+        "publicFieldsHash": "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
+        "allocatorSignature": "0xe7694921b02ebd3c44a50ed83fda61bfe2de576c06096f121c2a3fb7c8b6000e793c1968d0e7dc417d68cebf95bc0a757f3371828085787d53bd1afd64a83d601b",
+        "uHash": "0xd4b1ee22a7a2eeb2adf53d79b7430a396ffcf699276112fa21949b8ff8fd172c",
+        "validatorAddress": "0xb1C4C1E1Cdd5Cf69E27A3A08C8f51145c2E12C6a",
+        "validatorSignature": "0xcc97b2dc887e94013b7f4236b1412818065bf0ef94bbdb1b6b71a6b78d69d20852c834bbf3a123942a7386bac12ebd6da51a82217d269f9572aaa1422392ea111b",
+        "recipient": "0xecdFC9CA344CE8E71538aFDf05c49E5Cbcd84b1a"
       }
-      
-      const balanceBefore = await token.balanceOf(user1.address);
+            
       // Call claim function
       await dropERC20.connect(user1).claim(
         hexlify(toUtf8Bytes(webproof.taskId)),
         webproof.validatorAddress,
         webproof.uHash,
         webproof.publicFieldsHash,
+        webproof.recipient,
+        webproof.allocatorSignature,
+        webproof.validatorSignature
+      );
+      
+      const balanceAfter = await token.balanceOf(webproof.recipient);
+      expect(balanceAfter).to.equal(amount);
+    });
+
+    // Test 2: Successful claim with ephemeral key
+    it("should allow a valid user to claim tokens with ephemeral key", async function () {
+      const webproof = {
+        "taskId": "0cad2ec6ef3248a992d59e2315ce38d1",
+        "publicFields": [],
+        "allocatorAddress": "0x19a567b3b212a5b35bA0E3B600FbEd5c2eE9083d",
+        "publicFieldsHash": "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
+        "allocatorSignature": "0xe7694921b02ebd3c44a50ed83fda61bfe2de576c06096f121c2a3fb7c8b6000e793c1968d0e7dc417d68cebf95bc0a757f3371828085787d53bd1afd64a83d601b",
+        "uHash": "0xd4b1ee22a7a2eeb2adf53d79b7430a396ffcf699276112fa21949b8ff8fd172c",
+        "validatorAddress": "0xb1C4C1E1Cdd5Cf69E27A3A08C8f51145c2E12C6a",
+        "validatorSignature": "0xcc97b2dc887e94013b7f4236b1412818065bf0ef94bbdb1b6b71a6b78d69d20852c834bbf3a123942a7386bac12ebd6da51a82217d269f9572aaa1422392ea111b",
+        "recipient": "0xecdFC9CA344CE8E71538aFDf05c49E5Cbcd84b1a"
+      }
+
+      // this is a test key, do not import it and do not use it
+      const ephemeralKey = "3f152b434d72ee6fdfebfae22b5e398b08ca51668c645a2903fa89b616230591"
+      const ephemeralKeySig = await generateEphemeralKeySig(ephemeralKey, user1.address)
+
+      const balanceBefore = await token.balanceOf(user1.address);            
+      // Call claim function
+      await dropERC20.connect(user1).claimWithEphemeralKey(
+        hexlify(toUtf8Bytes(webproof.taskId)),
+        webproof.validatorAddress,
+        webproof.uHash,
+        webproof.publicFieldsHash,
         user1.address,
+        webproof.recipient,
+        ephemeralKeySig,
         webproof.allocatorSignature,
         webproof.validatorSignature
       );
@@ -72,6 +140,7 @@ describe("DropERC20", function () {
       expect(balanceAfter - balanceBefore).to.equal(amount);
     });
 
+    
     // Test 2: Prevent double claims
     xit("should prevent a user from claiming twice", async function () {
       // First claim should succeed
